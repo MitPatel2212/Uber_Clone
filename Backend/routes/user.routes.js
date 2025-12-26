e27a3bcba083f96cbeebb16c74c0b5b65260fab5
Patel Mit <patelmit4848@gmail.com>
2025-12-26 23:21:26 +0530

    Initial setup

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require("express-validator")
+const userController = require('../controllers/user.controller');
+  
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min: 3}).withMessage('first name must be at least 3 charecters long'),
+    body('password').isLength({min : 6}).withMessage('password must be atleast 6 charecters long')
+],   
+    userController.registerUser
+)
+
+
+module.exports = router;
